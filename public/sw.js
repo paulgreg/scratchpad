4ebabd8abc62f52cde2ca5e7828452ceb7768cc1
Grégory Paul <me@gpaul.me>
2025-04-18 18:58:34 +0200

    Revert "chore: intercepts expired token and ask for reload"
    This reverts commit 28e49050964df4004e4a00b94ac8ffc05b7f198b.

--- public/sw.js
+++ public/sw.js
@@ -15,15 +15,8 @@ function fromNetwork(request, timeout) {
   return new Promise((fulfill, reject) => {
     let timeoutId
     if (timeout) timeoutId = setTimeout(reject, timeout)
-    fetch(request).then(async (response) => {
+    fetch(request).then((response) => {
       if (timeout) clearTimeout(timeoutId)
-
-      if (response.redirected && response.url.includes('/vouch/login')) {
-        const allClients = await clients.matchAll()
-        for (const client of allClients) {
-          client.postMessage({ type: 'unauthorized' })
-        }
-      }
 
       if (!response || response.status !== 200 || request.method !== 'GET') {
         return fulfill(response)
